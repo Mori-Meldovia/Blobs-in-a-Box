d60c0733f07d52466423e3c2dba8ef581d2769e3
Alex Shen <shen46806@sas.edu.sg>
2024-10-20 18:25:29 +0800

    movement test 2

--- blobs-in-a-box/game.gd
+++ blobs-in-a-box/game.gd
@@ -4,20 +4,38 @@ extends Node
 var grid_size = 64
 #var grid = []
 var player = {
-	"pos": Vector2(3, 5)
+	"pos": Vector2i(1, 1),
+	"color": COLOR.BLUE
 }
 enum COLLIDE {
 	COLLIDE,
 	NONE,
 	KILL
 }
+enum COLOR {
+	RED,
+	BLUE
+}
+# fake enum lmao
+const CELL = {
+	"WALL_RED": Vector2i(0, 0),
+	"WALL_BLUE": Vector2i(1, 0),
+	"WALL": Vector2i(0, 1),
+	"FLOOR": Vector2i(1, 1)
+}
 
-func check_collision(pos: Vector2) -> int:
+func check_collision(coord: Vector2i, color: COLOR) -> int:
+	if ($Background.get_cell_atlas_coords(coord) == CELL.WALL):
+		return COLLIDE.COLLIDE
+	if ($Background.get_cell_atlas_coords(coord) == CELL.WALL_BLUE && color != COLOR.BLUE):
+		return COLLIDE.COLLIDE
+	if ($Background.get_cell_atlas_coords(coord) == CELL.WALL_RED && color != COLOR.RED):
+		return COLLIDE.COLLIDE
 	return COLLIDE.NONE
 
 # Converts grid coordinate to position vector
-func coord2pos(coord: Vector2) -> Vector2:
-	return grid_size * (coord + Vector2.ONE / 2)
+func coord2pos(coord: Vector2i) -> Vector2:
+	return grid_size * (Vector2(coord) + Vector2.ONE / 2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -33,14 +51,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Up"):
-		#if check_collision(Vector2(player.pos.x, player.pos.y - 1)) == COLLIDE.NONE:
-		player.pos.y -= 1
+		if check_collision(Vector2i(player.pos.x, player.pos.y - 1), player.color) == COLLIDE.NONE:
+			player.pos.y -= 1
 	if Input.is_action_just_pressed("Down"):
-		player.pos.y += 1
+		if check_collision(Vector2i(player.pos.x, player.pos.y + 1), player.color) == COLLIDE.NONE:
+			player.pos.y += 1
 	if Input.is_action_just_pressed("Left"):
-		player.pos.x -= 1
+		if check_collision(Vector2i(player.pos.x - 1, player.pos.y), player.color) == COLLIDE.NONE:
+			player.pos.x -= 1
 	if Input.is_action_just_pressed("Right"):
-		player.pos.x += 1
+		if check_collision(Vector2i(player.pos.x + 1, player.pos.y), player.color) == COLLIDE.NONE:
+			player.pos.x += 1
 	
 	#couldn't get interpolation to work
 	#$Sienna.position = Vector2(move_toward($Sienna.position.x, coord2pos(player.pos).x, delta), move_toward($Sienna.position.y, coord2pos(player.pos).y, delta))
