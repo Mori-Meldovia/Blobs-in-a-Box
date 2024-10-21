13a4ca6e5b93e72c63530376627bd187ebd0b83d
Alex Shen <shen46806@sas.edu.sg>
2024-10-21 22:20:52 +0800

    new textures, win condition

--- blobs-in-a-box/scripts/game.gd
+++ blobs-in-a-box/scripts/game.gd
@@ -1,8 +1,15 @@
 extends Node
 
 enum COLOR {
-	BLUE,
-	RED
+	BLUE = 1,
+	RED,
+	PURPLE # Most purple objects are just blue and red objects stacked on top of each other. However, goals should be explicitly purple because either the blue or red stepping into the goal should trigger it, whereas a blue and red goal will require both sprites to step into it. Compare colors using bitwise (color & COLOR.BLUE)
+}
+
+enum OBJECTS {
+	EMPTY = -1,
+	FLAG,
+	SKULL
 }
 
 enum MOVABLES {
@@ -10,18 +17,15 @@ enum MOVABLES {
 	PUSH
 }
 
-# variables specifically for the players
-var starting_blue_pos := Vector2i(1, 1)
-var starting_red_pos := Vector2i(16, 1) 
-var blue_player_moves : Array[Vector2i] = [starting_blue_pos]
-var red_player_moves : Array[Vector2i] = [starting_red_pos]
+const GRID_SIZE := 64
+const SPEED := 15
+
+var moves := 0
 
 var movables : Array[Dictionary]
+var flags := 0
+var stars := 0 # to be implemented
 
-const GRID_SIZE := 64
-@export var speed := 15
-var moves := 0
-var t := 1.
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -31,7 +35,7 @@ func _ready() -> void:
 			"node": $PlayerBlue,
 			"type": MOVABLES.PLAYER,
 			"color": COLOR.BLUE,
-			"pos": starting_blue_pos,
+			"pos": null,
 			"last_pos": null,
 			"moves": []
 		},
@@ -39,19 +43,31 @@ func _ready() -> void:
 			"node": $PlayerRed,
 			"type": MOVABLES.PLAYER,
 			"color": COLOR.RED,
-			"pos": starting_red_pos,
+			"pos": null,
 			"last_pos": null,
 			"moves": []
 		}
 	]
 	
+	# Initialise obj.pos, snap to grid, and add an initial move
 	for obj in movables:
 		obj.node.scale = Vector2.ONE * 0.45
+		obj.pos = pos2coord(obj.node.position)
 		obj.node.position = coord2pos(obj.pos)
 		obj.moves.append(obj.pos)
 	
+	# Make a count of all flags and stars
+	var search_size := DisplayServer.screen_get_usable_rect().size / (Vector2i.ONE * GRID_SIZE)
+	for y in range(search_size.y):
+		for x in range(search_size.x):
+			if $Objects.get_cell_atlas_coords(Vector2i(x, y)).x == 0:
+				# is a flag
+				flags += 1
+			# stars to be implemented
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var t := 1.
 func _process(delta: float) -> void:
 	if t >= 1:
 		# Store last position for interpolation
@@ -97,17 +113,35 @@ func _process(delta: float) -> void:
 			returnPos()
 		
 	else:
-		t = minf(t + delta * speed, 1)
+		t = minf(t + delta * SPEED, 1)
 	
-	# Update positions
 	for obj in movables:
+		# Update positions
 		obj.node.position = obj.last_pos.lerp(coord2pos(obj.pos), t * t * (3 - 2 * t))
+	
+	
+	# Check for objects
+	#to be implemented
+	#check_object(obj.pos, obj.color)
+	#if object is star, star--, destroy star
+	
+	# Win condition
+	var flags_left := flags
+	for obj in movables:
+		if check_object(obj.pos, obj.color) == OBJECTS.FLAG:
+			flags_left -= 1
+	if flags_left == 0 && stars == 0:
+		print("Game won!")
 	
 
 
 # Converts grid coordinate to position vector
 func coord2pos(coord: Vector2i) -> Vector2:
 	return GRID_SIZE * (Vector2(coord) + Vector2.ONE / 2)
+
+# Converts position vector to grid position. Used to fetch starting positions of movables
+func pos2coord(pos: Vector2) -> Vector2i:
+	return Vector2i(floor(pos / GRID_SIZE))
 
 # Checks for a wall collision
 func can_move(coord: Vector2i, color: COLOR) -> bool:
@@ -116,13 +150,22 @@ func can_move(coord: Vector2i, color: COLOR) -> bool:
 		return false
 	elif $Walls.get_cell_atlas_coords(coord).y < 8:
 		# red wall
-		if color == COLOR.RED:
+		if color & COLOR.RED:
 			return false
 	elif $Walls.get_cell_atlas_coords(coord).y < 12:
 		# blue wall
-		if color == COLOR.BLUE:
+		if color & COLOR.BLUE:
 			return false
 	return true
+
+# Checks for an object that is the same color (or purple)
+func check_object(coord: Vector2i, color: COLOR) -> OBJECTS:
+	var atlas : Vector2i = $Objects.get_cell_atlas_coords(coord)
+	if atlas.y == 1 && !(color & COLOR.RED) || atlas.y == 2 && !(color & COLOR.BLUE):
+		# non matching color
+		return OBJECTS.EMPTY
+	else:
+		return atlas.x # should correspond to OBJECTS
 
 # Dumps debug information
 func returnPos() -> void:
@@ -138,6 +181,8 @@ func returnPos() -> void:
 			color = "BLUE"
 		elif obj.color == COLOR.RED:
 			color = "RED"
+		elif obj.color == COLOR.PURPLE:
+			color = "PURPLE"
 		
 		print(type + " " + color + " | " + str(obj.pos))
 		print(obj.moves)
